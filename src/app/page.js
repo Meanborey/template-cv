be03e7e11c73c6650b49dbb9457ceab4264c1f6c
Meanborey <99602111+Meanborey@users.noreply.github.com>
2023-06-05 02:03:33 +0700

    upload again

--- src/app/page.js
+++ src/app/page.js
@@ -3,111 +3,141 @@ import Image from 'next/image'
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.js</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+        <div className='cv w-full min-h-96 bg-white grid grid-rows-1 grid-flow-col gap-4'>
+            <div className='col left_cv '>
+             
+              <div className='portimg  '>
+               
+               <img className='shadow-lg' src="https://d38b044pevnwc9.cloudfront.net/cutout-nuxt/enhancer/1.jpg" />
+               
+               <br/>
+               <h1 className='text-2xl text-white font-semibold'>SENG</h1>
+               <p className='text-2xl text-white font-semibold'>SEANG LENG</p>
+               <br/>
+               <h2 className='text-base text-white font-semibold'>Reference</h2> <br/>
+               <p className='text-xs text-white'><storng className=' text-white font-semibold'>Position :</storng>senior core banking </p>
+               <p className='text-xs text-white'><storng className=' text-white font-semibold'>Email :</storng>seth.rupp.@rupp.com </p>
+               <p className='text-xs text-white'><storng className=' text-white font-semibold'>Telephone :</storng>+855 843 345 44</p>
+
+              </div>
+
+            </div>
+            <div className='col right_cv '>
+              <h2 className='skill text-black font-semibold'>Experience</h2>
+              <p className='bg-black h-1 w-72'></p><br/>
+              <p className='texth '>IT Instructor – Position or Specialize (optional)</p>
+              
+                <li className='ml-5'>
+                    <a className='text_li'>Specialize on Blockchain, SQL & Data Modeling, Android, Flutter</a>
+                </li>
+                <li className='ml-5'>
+                     <a className='text_li'>Prepare course syllabus and material</a>
+                </li>
+                <li className='ml-5'>
+                    <a className='text_li'>Support Training Department</a>
+                </li>
+                <br/>
+                <p className='texth '>Senior UI/UX Designer </p>
+                
+                <li className='ml-5'>
+                    <a className='text_li'>Prepare the templates</a>
+                </li>
+                <li className='ml-5'>
+                     <a className='text_li'>Design website template </a>
+                </li>
+                <li className='ml-5'>
+                    <a className='text_li'>Design mobile template</a>
+                </li>
+                 <li className='ml-5'>
+                    <a className='text_li'>Support team with leader of the UI position</a>
+                </li>
+                <br/>
+                <h2 className='skill text-black font-semibold'>Experience</h2>
+                <p className='bg-black h-1 w-72'></p>
+                <br/>
+                <p className='texth '>Royal university of Phnom Penh - RUPP</p>
+                <p className='text_li'>2021 - 2024</p>
+
+                <li className='ml-5'>
+                    <a className='text_li'>C, C++ , JAVA programming language </a>
+                </li>
+                <li className='ml-5'>
+                     <a className='text_li'>Database management system (SQL, MySQL server )</a>
+                </li>
+                <li className='ml-5'>
+                    <a className='text_li'>Web - development</a>
+                </li>
+                 <li className='ml-5'>
+                    <a className='text_li'>Management information system</a>
+                </li>
+                <br/>
+                <p className='texth'>Art design - Website design</p>
+                <p className='text_li'>2021</p>
+
+                <li className='ml-5'>
+                     <a className='text_li'>Figma - design</a>
+                </li>
+                <li className='ml-5'>
+                    <a className='text_li'>Front - end design</a>
+                </li>
+                 <li className='ml-5'>
+                    <a className='text_li'>Editing photos</a>
+                </li>
+                <br/>
+                {/* <h2 className='skill text-black font-semibold'>Skills</h2>
+                <p className='bg-black h-1 w-72'></p>
+                <br/> */}
+
+
+
+             
+              
+
+            </div>
         </div>
-      </div>
+        <br/>
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
+        <div className='cv w-full min-h-96 bg-white grid grid-rows-1 grid-flow-col gap-4'>
 
-      <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
+            <div className='col left_cv  '>
+            <h2 className='text-base ml-6 mt-5 text-white font-semibold'>Contact Us</h2> 
+            <br/>
+             <p className='text-xs ml-6 text-white'><storng className=' text-white font-semibold'>Email :</storng> mr.leng@gmail.com</p>
+               <p className='text-xs ml-6 text-white'><storng className=' text-white font-semibold'>Address :</storng> St 562, Phnom Penh 12151</p>
+               <p className='text-xs ml-6 text-white'><storng className=' text-white font-semibold'>Telephone :</storng>+855 843 345 44</p>
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
 
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
+            </div>
 
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
+
+
+
+
+            <div className='col right_cv '>
+            <h2 className='skill text-black mt-3 font-semibold'>Skills</h2>
+                <p className='bg-black h-1 w-72'></p>
+                <br/>
+                <p className='text-xs'>Full stack development ( Pro )</p>
+
+                <li className='ml-5'>
+                    <a className='text_li'>Front - end</a>
+                </li>
+                 <li className='ml-5'>
+                    <a className='text_li'>Back - end</a>
+                </li>
+                <br/>
+                <h2 className='skill text-black font-semibold'>Language</h2>
+                <p className='bg-black h-1 w-72'></p>
+                <br/>
+                <p className='text-xs'>English</p>
+
+                <li className='ml-5'>
+                    <a className='text_li'>ACE IELTS teacher trainer</a>
+                </li>
+
+
+            </div>
+        </div>
     </main>
   )
 }
